5ffc8ed4a022424936c78c6cfb05518febb7e68f
JhostinGrande <92886364+JhostinGrande@users.noreply.github.com>
2021-10-21 14:27:52 -0600

    Cree algo no se que

---
+++ Pocho01/Dashboard1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Pocho01
+{
+    partial class Dashboard1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.DashboardCommon.ChartPane chartPane1 = new DevExpress.DashboardCommon.ChartPane();
+            DevExpress.DashboardCommon.DashboardLayoutGroup dashboardLayoutGroup1 = new DevExpress.DashboardCommon.DashboardLayoutGroup();
+            DevExpress.DashboardCommon.DashboardLayoutItem dashboardLayoutItem1 = new DevExpress.DashboardCommon.DashboardLayoutItem();
+            this.chartDashboardItem1 = new DevExpress.DashboardCommon.ChartDashboardItem();
+            ((System.ComponentModel.ISupportInitialize)(this.chartDashboardItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // chartDashboardItem1
+            // 
+            this.chartDashboardItem1.AxisX.TitleVisible = false;
+            this.chartDashboardItem1.ComponentName = "chartDashboardItem1";
+            this.chartDashboardItem1.DataItemRepository.Clear();
+            this.chartDashboardItem1.InteractivityOptions.IgnoreMasterFilters = false;
+            this.chartDashboardItem1.Name = "Gráfico 1";
+            chartPane1.Name = "Panel 1";
+            chartPane1.PrimaryAxisY.AlwaysShowZeroLevel = true;
+            chartPane1.PrimaryAxisY.ShowGridLines = true;
+            chartPane1.PrimaryAxisY.TitleVisible = true;
+            chartPane1.SecondaryAxisY.AlwaysShowZeroLevel = true;
+            chartPane1.SecondaryAxisY.ShowGridLines = false;
+            chartPane1.SecondaryAxisY.TitleVisible = true;
+            this.chartDashboardItem1.Panes.AddRange(new DevExpress.DashboardCommon.ChartPane[] {
+            chartPane1});
+            this.chartDashboardItem1.ShowCaption = true;
+            // 
+            // Dashboard1
+            // 
+            this.Items.AddRange(new DevExpress.DashboardCommon.DashboardItem[] {
+            this.chartDashboardItem1});
+            dashboardLayoutItem1.DashboardItem = this.chartDashboardItem1;
+            dashboardLayoutItem1.Weight = 100D;
+            dashboardLayoutGroup1.ChildNodes.AddRange(new DevExpress.DashboardCommon.DashboardLayoutNode[] {
+            dashboardLayoutItem1});
+            dashboardLayoutGroup1.DashboardItem = null;
+            dashboardLayoutGroup1.Orientation = DevExpress.DashboardCommon.DashboardLayoutGroupOrientation.Vertical;
+            this.LayoutRoot = dashboardLayoutGroup1;
+            this.Title.Text = "Tablero de control";
+            ((System.ComponentModel.ISupportInitialize)(this.chartDashboardItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.DashboardCommon.ChartDashboardItem chartDashboardItem1;
+    }
+}
